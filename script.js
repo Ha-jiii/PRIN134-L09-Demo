68f363bceb2a6cc2a55a6e72132d6f6cbf2a0bdb
Ha-jiii <usi2233095@mls.ceu.edu.ph>
2025-03-18 12:23:14 +0800

    Done act 9

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function() {
+ 
+    document.getElementById("highlight-ingredients").addEventListener("click", function() {
+      const ingredients = document.querySelectorAll("#recipe-ingredients li");
+      ingredients.forEach((item, index) => {
+        if (index % 2 === 0) {
+          item.classList.toggle("highlight");
+        }
+      });
+    });
+})
+
+document.getElementById("check-instructions").addEventListener("click", function() {
+    setTimeout(() => {
+      const instructions = document.querySelectorAll("#recipe-instructions li");
+      instructions.forEach(item => {
+        item.classList.add("is-done");
+      });
+    }, 3000);
+});
+
+document.getElementById("reset-instructions").addEventListener("click", function() {
+    const instructions = document.querySelectorAll("#recipe-instructions li");
+    instructions.forEach(item => {
+      item.classList.remove("is-done");
+    });
+});
+
+document.querySelector("h2.headers:nth-of-type(1)").addEventListener("click", function() {
+    this.classList.add("animate__animated", "animate__rubberBand");
+    setTimeout(() => {
+      this.classList.remove("animate__animated", "animate__rubberBand");
+    }, 1000);
+});
+
+document.querySelector("h2.headers:nth-of-type(2)").addEventListener("click", function() {
+    this.classList.add("animate__animated", "animate__rubberBand");
+    setTimeout(() => {
+      this.classList.remove("animate__animated", "animate__rubberBand");
+    }, 1000);
+});
+
+document.querySelector("img").addEventListener("click", function() {
+    this.classList.add("animate__animated", "animate__jello");
+    setTimeout(() => {
+      this.classList.remove("animate__animated", "animate__jello");
+    }, 1000);
+});
